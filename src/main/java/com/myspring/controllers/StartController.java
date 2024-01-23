78f8cd65b3533c907f7cc2ebe2fe521c0ced3e26
iggosha <iegolovkov@gmail.com>
2024-01-23 06:32:33 +0300

    + @RequestParam, + Model, thymeleaf

--- src/main/java/com/myspring/controllers/StartController.java
+++ src/main/java/com/myspring/controllers/StartController.java
@@ -1,18 +1,21 @@
 package com.myspring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class StartController {
 
-    @GetMapping("/hello")
-    public String helloPage() {
-        return "start/hello";
+    @GetMapping("/bank")
+    public String helloPage(@RequestParam(name = "bankName") String bankName, Model model) {
+        model.addAttribute("bankName", bankName);
+        return "start/bank";
     }
 
-    @GetMapping("/goodbye")
+    @GetMapping("/smart_house")
     public String goodbyePage() {
-        return "start/goodbye";
+        return "start/smart_house";
     }
 }
