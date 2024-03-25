b8374e31b44b83947609df5d22a3811036c2a4f3
iggosha <iegolovkov@gmail.com>
2024-03-25 23:49:24 +0300

    (+/-): Конфигурация переделана под Spring Boot

--- src/main/java/com/myspring/MySpringApplication.java
+++ src/main/java/com/myspring/MySpringApplication.java
@@ -2,23 +2,11 @@ package com.myspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.filter.HiddenHttpMethodFilter;
-
-import java.util.Collections;
 
 @SpringBootApplication
 public class MySpringApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MySpringApplication.class, args);
-    }
-
-    @Bean
-    public FilterRegistrationBean hiddenHttpMethodFilter() {
-        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new HiddenHttpMethodFilter());
-        filterRegistrationBean.setUrlPatterns(Collections.singletonList("/*"));
-        return filterRegistrationBean;
     }
 }
