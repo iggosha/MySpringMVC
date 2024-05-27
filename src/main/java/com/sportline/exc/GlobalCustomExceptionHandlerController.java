d02a30d577981e77de875777fd8b339aaf821de6
iggosha <iegolovkov@gmail.com>
2024-05-27 04:35:21 +0300

    (+): Пример обработки RuntimeException-ов в отдельном контроллере

--- src/main/java/com/sportline/exc/GlobalCustomExceptionHandlerController.java
+++ src/main/java/com/sportline/exc/GlobalCustomExceptionHandlerController.java
@@ -1,5 +1,6 @@
 package com.sportline.exc;
 
+import com.sportline.exc.custom.CustomNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,12 +8,12 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalCustomExceptionHandlerController {
 
     @ExceptionHandler(CustomNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public String handleNotFound404(CustomNotFoundException exc, Model model) {
-        model.addAttribute("exc", exc);
+        model.addAttribute("em", exc.message);
         return "sportline/basic/exc";
     }
 }
